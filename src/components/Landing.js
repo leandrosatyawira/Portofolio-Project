990643439148388df107bb85f56a0e84d59957be
leandrosatyawira <169057802+leandrosatyawira@users.noreply.github.com>
2024-11-28 16:09:11 +0700

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,7 +10,7 @@ function LandingPage({ scrollToContact }) {
       </div>
       <div className="landing-content">
         <h1>Hello, I'm <span className="name-highlight">Leandro Satyawira Tionanda</span><span className="blinking-cursor">|</span></h1>
-        <p className="roles">Web Developer | Novice Programmer</p>
+        <p className="roles">Web Developer | Programmer</p>
         <button onClick={scrollToContact} className="contact-button">Contact Me</button>
       </div>
     </section>
